0edcd83fb2e7d3a9273fe440c12d5d2f6a6db9a3
unknown <leonlangat179@gmail.com>
2026-03-16 14:28:13 +0300

    add products ui

--- src/components/Addproducts.jsx
+++ src/components/Addproducts.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Loader from './Loader';
+import axios from 'axios';
+import { unstable_setDevServerHooks } from 'react-router-dom';
 
 const Addproducts = () => {
+
+  const[product_name, setProductname] = useState("");
+   const[product_description, setProductDescription] = useState("");
+    const[product_cost, setProductCost] = useState("");
+    const[product_photo, setProductPhoto] = useState("");
+  //declare the additionalhook to manage the state of application
+
+  const [loading, setLoading] =useState(false);
+  const [success, setSuccess] =useState("")
+  const [error, setError] =useState("")
+
+  // create a function that will handle the submit function
+  const handleSubmit = async(e) =>{
+    //prevent the site from reloading
+    e.preventDefault()
+
+    //setloading hook with a message(activate it)
+    setLoading(true)
+    //  <h3 className='text-secondary'>{success}</h3>
+    //   <h4 className='text-danger'>{error}</h4>
+    try{
+
+      //create a form data
+      const formdata = new FormData()
+
+      //appendthe details to the form data created
+      formdata.append("product_name", product_name);
+      formdata.append("product_description", product_description);
+      formdata.append("product_cost", product_cost);
+      formdata.append("product_photo",product_photo);
+
+      // interact with axios to help you use the method post
+      const response = await axios.post("https://leonlangat.alwaysdata.net/api/add_product", formdata)
+
+      // set the loading hook back to default
+      setLoading(false)
+
+      // update the success  hook with a message
+      setSuccess(response.data.message)
+      // clearing the hooks
+      setProductname("");
+      setProductDescription("");
+      setProductCost("");
+      setProductPhoto("");
+
+    }
+    catch(error){
+      //set loading hook back to default
+      setLoading(false)
+
+      //update the seterror with a message
+      setError(error.message)
+
+
+    }
+  }
+
   return (
-    <div>
-      <h1>Welcome to the add products page</h1>
+    <div className='row justify-content-center mt-4'>
+      <div className="col-md-6 card shadow p-4  ">
+        <h3 className='text-info'>Welcome to add products</h3>
+        {/* bind the loading hook */}
+        {loading && <Loader />}
+
+         <h3 className='text-secondary'>{success}</h3>
+      <h4 className='text-danger'>{error}</h4>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+        placeholder='Enter the product name'
+        className='form-control'
+        required
+        value={product_name}
+        onChange={(e) => setProductname(e.target.value)}
+        />
+        <br />
+        {/* {product_name} */}
+
+        <input type="text"
+        placeholder='Enter the product description'
+        className='form-control'
+        required
+        value={product_description}
+        onChange={(e) =>setProductDescription(e.target.value)}
+        /> <br />
+
+        {/* {product_description} */}
+
+        <input type="number"
+        placeholder='Enter the price of the product'
+        className='form-control'
+        required
+        value={product_cost}
+        onChange={(e) => setProductCost(e.target.value)}
+        /> <br />
+
+
+        {/* {product_cost} */}
+
+        <label >Product photo</label>
+        <input type="file" 
+        className='form-control'
+        required
+        accept='image/*'
+        onChange={(e) => setProductPhoto(e.target.files[0])}
+        /> <br />
+
+
+        <input type="submit"
+        value="Add Products"
+        className='btn btn-outline-primary'/>
+  
+        </form>
+      </div>
+      
     </div>
   )
 }
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Signin = () => {
+
+
+//define the two hooks for capturing /storing the users input
+
+const[email, setEmail] = useState("");
+const[password, setPassword] =useState("");
+
+//declare the three additional hooks
+const[loading,setLoading] =useState("");
+const[success,setSuccess] =useState("");
+const[error,setError] =useState("");
+
+//below we have the usenavigate hook to redirect us to another page on successful signin/login
+const navigate = useNavigate()
+
+//below is the function to handle submit
+const handlesubmit = async (e) =>{
+  e.preventDefault()
+  // update the loading hook with a message
+  setLoading("please wait while we authenticate your account.........")
+  
+  try{
+    //create a formdata object that will hold the email and the password
+    const formdata = new FormData()
+
+    // 10. Insert/append the email and the password on the formData created.
+    formdata.append("email", email)
+    formdata.append("password", password)
+
+    // interact with axios on response
+    const response = await axios.post("https://leonlangat.alwaysdata.net/api/signin", formdata)
+
+    //set loading hook back to default
+    setLoading("")
+
+    //check whether the user exists as part of your response from the API
+    if(response.data.user){
+      //if user is there,definately the details entered during signin are correct
+      // setSuccess("Login successful")
+      //if it is successful let a person redirected to another page
+
+      // Store user details in local storage
+    localStorage.setItem("user", JSON.stringify(response.data.user));
+
+
+      navigate("/");
+    }
+    else{
+      //user is not found that means the credential entered on the form are incorrect
+      setError("Login Failed.Please try again...")
+    }
+}
+catch(error)
+{
+//set loading back to default
+setLoading("")
+//update the error hook with a message
+setError("Oops, Something went wrong.Try Again ...")
+}
+}
+
+
   return (
-    <div>
-      <h1>Welcome to the signin page</h1>
+    <div className='row justify-content-center mt-4'>
+      <div className="col-md-6 card shadow p-4">
+        <h1 className='text-info'>Sign In</h1>
+
+        <h5 className='text-info'>{loading}</h5>
+        <h3 className='text-success'>{success}</h3>
+        <h3 className='text-danger'>{error}</h3>
+
+
+        <form onSubmit={handlesubmit}>
+          <input type="email" 
+          placeholder='Enter the Email adress here....'
+          className='form-control'
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          /> <br />
+
+          {/* {email} */}
+
+
+           <input type="password" 
+          placeholder='Enter your password here....'
+          className='form-control'
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+
+          /> <br />
+           <br /> 
+
+          {/* {password} */}
+
+
+
+          <input type="submit"
+          value="Signin"
+          className='btn btn-primary' />
+
+Dont have an Account? <Link to={'/signup'}>Register</Link> 
+            </form>
+      </div>
     </div>
   )
 }
